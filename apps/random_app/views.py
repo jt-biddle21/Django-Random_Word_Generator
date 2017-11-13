35fe5b664d8664e5a464785f88a19a1acaec0269
Justin Biddle <justinbiddle@Justins-MacBook-Air.local>
2017-11-13 11:05:44 -0800

    Created a random string generator

---
+++ apps/random_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
+
+
+def index(request):
+    return render(request, 'random_app/index.html')
+
+
+def generator(request):
+    if request.method == "POST":
+        print "posting"
+        random_word = get_random_string(length=15)
+        request.session['rand'] = random_word
+        request.session['count'] += 1
+        return redirect('/')
